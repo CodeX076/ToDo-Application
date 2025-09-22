5a3bb7c6bb400632e230d6d394e11b459631a1bf
Amit <youremail@example.comamitkrs2004@gmail.com>
2025-09-22 10:24:45 +0530

    Updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Dashboard from "./components/Dashboard";
-import Loader from "./components/Loader";
+
 
 function App() {
   
